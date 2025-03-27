58bcfde9a7651cfff10d5fddb039f075c842a58a
weedtech2025 <herbyking9@gmail.com>
2025-03-27 17:14:57 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "221766739821"
 
-global.bankowner = "FADARE"
+global.bankowner = "OLAWUNMI"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
